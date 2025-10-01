eb91b47930f74cd8a53f5d5af7d7acb25ece87a0
julianochies <julianopchiess@gmail.com>
2025-10-01 12:36:36 -0300

    Esqueleto do frontend

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function HomePage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default HomePage;
